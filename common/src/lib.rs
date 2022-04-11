f23e9f18cd7c50892f392a764361d6998dc38ce1
lorenzop <lorenzo@poixson.com>
2022-04-10 21:13:36 -0400

    added lib::load_template() function

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,18 @@
 
-pub mod utils;
+use std::fs::read_to_string;
+use handlebars::Handlebars;
 
+pub mod utils;
 pub mod file_finder;
+
+
+
+pub fn load_template(path: String, file: String) -> Handlebars<'static> {
+	let path_file = format!("{}/{}", path.clone(), file.clone());
+	let contents = read_to_string(path_file.clone())
+		.unwrap_or_else(|e| panic!("Failed to load template file: {} {}", path_file, e));
+	let mut bars = Handlebars::new();
+	bars.register_template_string(&file, contents.clone())
+		.unwrap_or_else(|e| panic!("Failed to parse template file: {} {}", path_file, e));
+	bars
+}
